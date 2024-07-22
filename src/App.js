5792f9bdc8fd09746ef34ff8db6a422bce4471c1
tyrants666 <akashyadav.iiita@gmail.com>
2024-07-23 02:13:13 +0545

    Toggle reminder updated to task on server

--- src/App.js
+++ src/App.js
@@ -26,6 +26,13 @@ function App() {
     // console.log(data);
   }
 
+  //Fetch singlular Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    return data
+  }
+
   //Add Task
   const addTask = async (task) => {
     const res = await fetch('http://localhost:5000/tasks',{ //This return added task from server
@@ -46,9 +53,19 @@ function App() {
   }
 
   //Toggle Reminder
-  const toggleReminder = id => {
+  const toggleReminder = async id => {
+    const taskToToggle = await fetchTask(id)
+    const updateTask = { ...taskToToggle, reminder: !taskToToggle.reminder}
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, { //This return added task from server
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updateTask)
+    })
+    const data = await res.json()
     setTasks(tasks.map( task => 
-      task.id === id ? { ...task, reminder: !task.reminder} : task
+      task.id === id ? { ...task, reminder: data.reminder} : task
     ))
   }
 
